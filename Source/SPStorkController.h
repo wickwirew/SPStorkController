6d407022f907776f6ba828365a4670e6fbe4d8e0
Wesley Wickwire <wickwirew@gmail.com>
2018-12-30 11:11:47 -0500

    added xcode project and shared schemes

---
+++ Source/SPStorkController.h
@@ -0,0 +1,19 @@
+//
+//  SPStorkController.h
+//  SPStorkController
+//
+//  Created by Wes Wickwire on 12/30/18.
+//  Copyright © 2018 IvanVorobei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPStorkController.
+FOUNDATION_EXPORT double SPStorkControllerVersionNumber;
+
+//! Project version string for SPStorkController.
+FOUNDATION_EXPORT const unsigned char SPStorkControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPStorkController/PublicHeader.h>
+
+
